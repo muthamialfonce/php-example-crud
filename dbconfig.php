c8d9171a5264184594be8086c87c92139bfdce17
alfonce <alfonce@beautyclick.co.ke>
2017-03-16 21:16:47 +0300

    adding updated files

---
+++ dbconfig.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database ="crud_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$database);
+
+?> 
